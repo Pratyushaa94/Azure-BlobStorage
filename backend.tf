704a3a7405339748521f0adf6093fb891029cbea
Pratyushaa94 <prathyusha9449@gmail.com>
2025-07-11 17:09:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-of-carbon"              
     storage_account_name = "storage88998898"           
-    container_name       = "container55793"            
+    container_name       = "container5579"            
     key                  = "terraform.tfstate"
   }
 }
